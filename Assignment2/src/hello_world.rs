4a8e840e414761de42d92a52cabd2faaed9d41a9
Ankit Mogha <ankit.mogha@knoldus.com>
2021-02-21 15:29:16 +0530

    Assignment-2

---
+++ Assignment2/src/hello_world.rs
@@ -0,0 +1,3 @@
+pub fn printHelloWorld(){
+    println!("Hello World");
+}
---
+++ Assignment2/src/main.rs
@@ -0,0 +1,7 @@
+mod hello_world;
+mod fibonacci;
+
+fn main() {
+  hello_world::printHelloWorld();
+  fibonacci::fibo_series();  
+}
